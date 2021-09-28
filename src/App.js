b06912f7064ee542d19c1a58b27ec099d699f444
Sabin <baniya.sabinn@gmail.com>
2021-09-28 18:37:33 +0545

    Dark mode added in whole page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm'
 
 const App = () => {
+
+    const [mode, setmode] = useState('dark');
+
+    const toggleMode = () => {
+        if(mode === 'dark'){
+            setmode('light')
+            document.body.style.backgroundColor = 'white';
+            document.body.style.color = 'black';            
+        }else{
+            setmode('dark')
+            document.body.style.backgroundColor = '#10181f';
+            document.body.style.color = 'white';
+        }
+    }
+
     return (
         <>
-        <Navbar title="TextUtils" aboutText="About TextUtils"/>
+        <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}/>
         <div className="container">
-            <TextForm heading="Enter your text below: "/>
+            <TextForm heading="Enter your text below: " mode={mode}/>
         </div>
         {/* // <About/> */}
         </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -46,14 +46,14 @@ function TextForm(props) {
         <div className="container">
             <div className="mb-3 my-3">
                 <h3>{props.heading}</h3>
-                <textarea className="form-control my-3" id="myText" rows="5" value={text} onChange={buttonOnChange}></textarea>
-                <button className="btn btn-primary mx-2" onClick={buttonUpClick}>Convert to Uppercase</button>
-                <button className="btn btn-primary mx-2" onClick={buttonLoClick}>Convert to Lowercase</button>
-                <button className="btn btn-primary mx-2" onClick={buttonCapsClick}>First Letter Caps</button>
-                <button className="btn btn-primary mx-2" onClick={buttonCopy}>Copy Text to Clipboard</button>
-                <button className="btn btn-primary mx-2" onClick={buttonRemoveExtraSpaces}>Remove Extra Spaces</button>
+                <textarea className="form-control my-3" id="myText" rows="5" value={text} onChange={buttonOnChange} style={{backgroundColor: props.mode==='dark'?'#10181f':'white', color: props.mode==='dark'?'white':'black'}}></textarea>
+                <button className="btn btn-primary mx-2 my-2" onClick={buttonUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-2 my-2" onClick={buttonLoClick}>Convert to Lowercase</button>
+                <button className="btn btn-primary mx-2 my-2" onClick={buttonCapsClick}>First Letter Caps</button>
+                <button className="btn btn-primary mx-2 my-2" onClick={buttonCopy}>Copy Text to Clipboard</button>
+                <button className="btn btn-primary mx-2 my-2" onClick={buttonRemoveExtraSpaces}>Remove Extra Spaces</button>
                 
-                <button className="btn btn-danger mx-2" onClick={buttonClear}>Clear</button>
+                <button className="btn btn-danger mx-2 my-2" onClick={buttonClear}>Clear</button>
 
             </div>
         </div>
